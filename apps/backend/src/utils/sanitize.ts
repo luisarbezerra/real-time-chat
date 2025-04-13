5fa73f6c7cfd8722bfd431a8834bbad5960cb91d
Luísa Ribeiro Bezerra <luisarbezerra@gmail.com>
2025-04-13 19:25:53 +0200

    chore: update backend code

--- apps/backend/src/utils/sanitize.ts
+++ apps/backend/src/utils/sanitize.ts
@@ -1,13 +1,7 @@
 import xss from 'xss';
 
-export const xssOptions = {
-  whiteList: {}, // empty whitelist means no tags are allowed
-  stripIgnoreTag: true, // strip HTML tags not in whitelist
-  stripIgnoreTagBody: ['script'], // strip script tags and their content
-};
-
 export const sanitizeText = (text: string): string => {
-  return xss(text, xssOptions);
+  return xss(text);
 };
 
 export const sanitizeUser = (user: { name: string; id: string }) => {
